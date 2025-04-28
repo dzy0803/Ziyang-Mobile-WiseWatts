ab67d5882c8f1f2b6c6bc843f9def219d996da8b
Ziyang Deng <dengziyang84@gmail.com>
2025-04-28 04:32:27 +0100

    splash page completed (version 1.0)

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,251 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(WiseWattsApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class WiseWattsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'WiseWatts',
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => SplashPage(),
+        '/home': (context) => HomePage(),
+        '/deviceDetail': (context) => DeviceDetailPage(),
+      },
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class SplashPage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _SplashPageState createState() => _SplashPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _SplashPageState extends State<SplashPage> with SingleTickerProviderStateMixin {
+  double _opacity = 0.0;
+  double _progress = 0.0; // Progress value for the loading bar
+  bool _isIndeterminate = true; // Whether the loading bar is indeterminate
+  bool _showCheck = false; // Whether to show the checkmark animation
+  double _checkOpacity = 0.0; // Opacity of the checkmark animation
 
-  void _incrementCounter() {
+  late AnimationController _haloController;
+  late Animation<double> _haloSizeAnimation;
+  late Animation<double> _haloOpacityAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Initialize halo animation controller
+    _haloController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    );
+
+    _haloSizeAnimation = Tween<double>(begin: 0, end: 200).animate(
+      CurvedAnimation(parent: _haloController, curve: Curves.easeOut),
+    );
+
+    _haloOpacityAnimation = Tween<double>(begin: 0.5, end: 0).animate(
+      CurvedAnimation(parent: _haloController, curve: Curves.easeOut),
+    );
+
+    // Logo fade-in
+    Future.delayed(Duration(milliseconds: 300), () {
+      setState(() {
+        _opacity = 1.0;
+      });
+    });
+
+    // First 2 seconds: keep indeterminate loading
+    Future.delayed(Duration(seconds: 2), () {
+      setState(() {
+        _isIndeterminate = false;
+      });
+      _simulateLoading();
+    });
+  }
+
+  void _simulateLoading() async {
+    // Quickly fill the loading bar
+    for (int i = 0; i <= 100; i++) {
+      await Future.delayed(Duration(milliseconds: 5));
+      setState(() {
+        _progress = i / 100;
+      });
+    }
+
+    // After loading is complete, show checkmark
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _showCheck = true;
+    });
+
+    Future.delayed(Duration(milliseconds: 200), () {
+      setState(() {
+        _checkOpacity = 1.0;
+      });
+      _haloController.forward(); // Play halo expansion
+    });
+
+    // Navigate to Home after 2 seconds
+    Future.delayed(Duration(seconds: 2), () {
+      Navigator.pushReplacementNamed(context, '/home');
     });
   }
 
   @override
+  void dispose() {
+    _haloController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    double screenWidth = MediaQuery.of(context).size.width;
+    double logoSize = screenWidth * 0.6;
+
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      backgroundColor: Colors.white,
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            // Logo fade-in
+            AnimatedOpacity(
+              opacity: _opacity,
+              duration: Duration(seconds: 2),
+              curve: Curves.easeInOut,
+              child: Image.asset(
+                'assets/images/logo.png',
+                width: logoSize,
+                height: logoSize,
+                fit: BoxFit.contain,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            SizedBox(height: 40),
+            // Loading bar
+            AnimatedOpacity(
+              opacity: _opacity,
+              duration: Duration(seconds: 2),
+              child: Container(
+                width: screenWidth * 0.6,
+                height: 6,
+                decoration: BoxDecoration(
+                  color: Colors.orangeAccent,
+                  borderRadius: BorderRadius.circular(3),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(3),
+                  child: _isIndeterminate
+                      ? LinearProgressIndicator(
+                          backgroundColor: Colors.grey.shade300,
+                          valueColor: AlwaysStoppedAnimation<Color>(Colors.orangeAccent),
+                        )
+                      : LinearProgressIndicator(
+                          value: _progress,
+                          backgroundColor: Colors.grey.shade300,
+                          valueColor: AlwaysStoppedAnimation<Color>(Colors.orangeAccent),
+                        ),
+                ),
+              ),
+            ),
+            SizedBox(height: 60),
+            // Fixed area for checkmark and halo animation
+            Container(
+              width: 120,
+              height: 120,
+              child: _showCheck
+                  ? Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        AnimatedBuilder(
+                          animation: _haloController,
+                          builder: (context, child) {
+                            return Container(
+                              width: _haloSizeAnimation.value,
+                              height: _haloSizeAnimation.value,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.orangeAccent.withOpacity(_haloOpacityAnimation.value),
+                              ),
+                            );
+                          },
+                        ),
+                        AnimatedOpacity(
+                          opacity: _checkOpacity,
+                          duration: Duration(seconds: 2),
+                          child: Lottie.asset(
+                            'assets/lottie/tickorange.json',
+                            repeat: false,
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                      ],
+                    )
+                  : SizedBox.shrink(), // Placeholder to maintain layout
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+// Home Page
+class HomePage extends StatelessWidget {
+  final List<String> deviceList = ['Fridge', 'Air Conditioner', 'Washer'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('WiseWatts - Home'),
+      ),
+      body: ListView.builder(
+        itemCount: deviceList.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(deviceList[index]),
+            trailing: Icon(Icons.chevron_right),
+            onTap: () {
+              Navigator.pushNamed(
+                context,
+                '/deviceDetail',
+                arguments: deviceList[index],
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+// Device Detail Page
+class DeviceDetailPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final String deviceName = ModalRoute.of(context)!.settings.arguments as String;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('$deviceName Detail'),
+      ),
+      body: Center(
+        child: Text(
+          'Details for $deviceName',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
     );
   }
 }
